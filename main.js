9e102792b8892568ee5b2aad2df8ab3ca6458eca
Neaxic <36155981+Neaxic@users.noreply.github.com>
2021-10-22 00:56:12 +0200

    ...

--- main.js
+++ main.js
@@ -21,12 +21,10 @@ const blacklist = require('./commands/blacklist')
 client.once('ready', () => {
     console.log("Bot-man is online");
 
-    client.on('ready', () => {
-        const botChannelID = '883827218923528234'
-        const botChannel = await client.channels.fetch(botChannelID)
-        var timestamp = Date();
-        botChannel.sendMessage(`Im alive and idk how - ${timestamp}`)
-    })
+    const botChannelID = '883827218923528234'
+    const botChannel = await client.channels.fetch(botChannelID)
+    var timestamp = Date();
+    botChannel.sendMessage(`Im alive and idk how`)
 
     pingCheck(client)
 
