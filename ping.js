d7b812c925656b4eb9a4812456b706ca67832b66
Neaxic <36155981+Neaxic@users.noreply.github.com>
2021-10-22 01:33:06 +0200

    i

--- ping.js
+++ ping.js
@@ -1,9 +1,8 @@
 module.exports = (client) => {
-    client.on('message', async (msg) => {
+    client.on('message', (msg) => {
         if (msg.content === ".ping") {
             msg.react('👌');
-            var responseMsg = await msg.reply('... Calculating ...');
-            responseMsg.edit(`🏓Latency is ${Date.now() - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+            msg.reply(`🏓Latency is ${Date.now() - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
         }
     })
 }
